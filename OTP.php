48507117fb833e0f4b278863ad74bc3c99b2b3cd
Ashish Garg <gashish641@gmail.com>
2021-09-08 12:57:32 +0530

    otp and confirmation files styled

--- OTP.php
+++ OTP.php
@@ -1,9 +1,7 @@
 <?php
-
-
 error_reporting(E_ERROR | E_PARSE);
 
-$con = mysqli_connect("localhost", "root", "Smarthyoyo9873", "CivilDisobedience");
+$con = mysqli_connect("localhost", "root", "", "CivilDisobedience");
 if (!$con) {
     die('Could not connect: ' . mysqli_connect_error());
 }
@@ -66,12 +64,8 @@ if (isset($_POST['submit'])) {
     require 'PHPMailer-master/PHPMailerAutoload.php';
 
     $mail = new PHPMailer();
-
-
     $mail->SMTPDebug = 0;
-
     $mail->isSMTP();
-
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPOptions = array(
         'ssl' => array(
@@ -97,7 +91,6 @@ if (isset($_POST['submit'])) {
 
     $mail->isHTML(true);
 
-
     $mail->Subject = "OTD Complaint Form OTP";
     $mail->Body = "<i>Your One Time Password is: </i>" . $code;
     $mail->AltBody = "OTP: ";
@@ -117,32 +110,93 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <title>OTP</title>
     <style>
-        <?php include 'OTP.css' ?>
+        #emblem {
+			margin-left: 2.5em;
+			width: 80%;
+		}
     </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="left">
-            <div class="header">
-                <h2 class="animation a1">OTP Confirmation</h2>
-                <h4 class="animation a2">Please, Enter the OTP send to the registered email<br><b><?php echo $Email_Address ?></b></h4>
-            </div>
-            <form action="OTP2.php" method="post">
-                <div class="form">
-                    <input type="text" class="form-field animation a3" placeholder="Enter OTP" name="captcha" required><br>
-                    <a>
-                        <button class="animation a5">CONFIRM</button>
-                    </a>
+    <!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light my_nav sticky-top">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="./admin.php"><strong>OnDuty Admin</strong></a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" aria-current="page" href="./index.php">Home</a>
+					</li>
+				</ul>
+				<div class="d-flex mx-2">
+					<button class="btn btn-light mx-1" type="submit"><a class="text-dark" href="./logout.php">Logout</a></button>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+    <div class="container main_container">
+        <div class="row my-5 justify-content-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="card" style="height: 100%;">
+                    <div class="card-body">
+                        <div class="card-title py-3">
+                            <h1>OTP Confirmation</h1>
+                            <p class="card-text text-muted">Please, Enter the OTP send to the registered email<?php echo $Email_Address ?></p>
+                            <form action="OTP2.php" method="post" class="mt-5">
+                                <div class="mb-3">
+                                    <input type="text" class="form-control form-control-lg" placeholder="Enter OTP" name="captcha">
+                                </div>
+                                <div class="mb-3">
+                                    <button class="btn btn-primary btn-block">CONFIRM</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </form>
-        </div>
-        <div class="right">
-            <img src="images/emblem.jpg" alt="emblem" id="image">
+            </div>
+            <div class="col-lg-6 col-md-6">
+				<div class="container">
+					<img src="./IndianEmblem.jpg" alt="Indian Emblem" id="emblem">
+				</div>
+			</div>
         </div>
     </div>
+
+    <!-- footer -->
+	<footer class="mt-5 my_footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-8">
+					<h3>OnDuty</h3>
+					<p>Your Safety, Our Priority.</p>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<h4>Contact Us </h4>
+					<p><Strong>Email : </Strong> otd@gmail.com</p>
+					<p><strong>Call us : </strong> +91 123456</p>
+					<h4>About Us </h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus veritatis, sunt rerum velit eaque earum ex</p>
+				</div>
+			</div>
+			<div class="col-lg-12">
+				<div class="copy text-center">
+					<p><small>Copyright &copy; 2020. All Rights Reserved</small></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+    <!-- Bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- OTP2.php
+++ OTP2.php
@@ -1,32 +1,27 @@
 <?php
 session_start();
 
-$code="";
+$code = "";
 
-if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"])
-{
+if (isset($_POST["captcha"]) && $_POST["captcha"] != "" && $_SESSION["code"] == $_POST["captcha"]) {
 
-$con = mysqli_connect("localhost", "root", "Smarthyoyo9873");
-      if (!$con) {
-          die('Could not connect: ' . mysqli_connect_error());
-      } else {
-    
-      }
-
-      //Database Selection
-      if (mysqli_select_db($con, "CivilDisobedience")) {
-    
-      } else {
-          die("Error in Selecting the Database <br>". mysqli_errno($con));
-      }
-
-      $mysql="UPDATE Complaints SET Complaint_Verified= true WHERE Complaint_Verified=false";
-
-      if (!mysqli_query($con, $mysql)) {
-        die('Error: ' . mysqli_error($con));
+    $con = mysqli_connect("localhost", "root", "Smarthyoyo9873");
+    if (!$con) {
+        die('Could not connect: ' . mysqli_connect_error());
+    } else {
     }
 
+    //Database Selection
+    if (mysqli_select_db($con, "CivilDisobedience")) {
+    } else {
+        die("Error in Selecting the Database <br>" . mysqli_errno($con));
+    }
 
+    $mysql = "UPDATE Complaints SET Complaint_Verified= true WHERE Complaint_Verified=false";
+
+    if (!mysqli_query($con, $mysql)) {
+        die('Error: ' . mysqli_error($con));
+    }
 ?>
 
     <!DOCTYPE html>
@@ -36,79 +31,193 @@ $con = mysqli_connect("localhost", "root", "Smarthyoyo9873");
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
         <title>Confirmartion</title>
         <style>
-            <?php include 'conformation.css' ?>
+            #emblem {
+                margin-left: 2.5em;
+                width: 80%;
+            }
         </style>
     </head>
 
     <body>
-        <div class="container">
-            <div class="left">
-                <div class="header">
-                    <h2 class="animation a1">Confirmation</h2>
-                    <br>
-                    <h4 class="animation a2">Hey, Your complaint is successfully filed with the portal.
-    We will take neccessary action and get back to you as soon as possible.
-    You would be notified when a officer would be assigned to your case.</h4>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light my_nav sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./admin.php"><strong>OnDuty Admin</strong></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex mx-2">
+                        <button class="btn btn-light mx-1" type="submit"><a class="text-dark" href="./logout.php">Logout</a></button>
+                    </div>
                 </div>
-                    <a href="index.php">
-                        <button class="animation a5">Back to Home Page</button>
-                    </a>
             </div>
-            <div class="right">
-                <img src="images/emblem.jpg" alt="emblem" id="image">
+        </nav>
+
+
+        <div class="container main_container">
+            <div class="row my-5 justify-content-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="card" style="height: 100%;">
+                        <div class="card-body">
+                            <div class="card-title py-3">
+                                <h1>Confirmation</h1>
+                            </div>
+                            <div class="card-text">
+                                <h3>Hey, Your complaint is successfully filed with the portal.
+                                    We will take neccessary action and get back to you as soon as possible.
+                                    You would be notified when a officer would be assigned to your case.</h3>
+                            </div>
+                            <a href="./index.php" class="btn btn-primary">Back to Home Page</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="container">
+                        <img src="./IndianEmblem.jpg" alt="Indian Emblem" id="emblem">
+                    </div>
+                </div>
             </div>
         </div>
+
+        <!-- footer -->
+        <footer class="mt-5 my_footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8">
+                        <h3>OnDuty</h3>
+                        <p>Your Safety, Our Priority.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <h4>Contact Us </h4>
+                        <p><Strong>Email : </Strong> otd@gmail.com</p>
+                        <p><strong>Call us : </strong> +91 123456</p>
+                        <h4>About Us </h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus veritatis, sunt rerum velit eaque earum ex</p>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="copy text-center">
+                        <p><small>Copyright &copy; 2020. All Rights Reserved</small></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Bootstrap js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     </body>
 
     </html>
 
 <?php
-
-include 'db.php';
-
-session_destroy();
-}
-else
-{
-
+    include 'db.php';
+    session_destroy();
+} else {
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <title>OTP</title>
+        <style>
+            #emblem {
+                margin-left: 2.5em;
+                width: 80%;
+            }
+        </style>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OTP</title>
-    <style>
-        <?php include 'OTP2.css' ?>
-    </style>
-</head>
-
-<body>
-    <div class="container">
-        <div class="left">
-            <div class="header">
-                <h2 class="animation a1">OTP Confirmation</h2>
-                <h4 class="animation a2">Incorrect OTP entered </h4>
-            </div>
-            <form action="conformation.php" method="post">
-                <div class="form">
-                    <input type="text" class="form-field animation a3" placeholder="Enter OTP" name="captcha" required>
-                    <a href="conformation.php">
-                        <button class="animation a5">CONFIRM</button>
-                    </a>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light my_nav sticky-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="./admin.php"><strong>OnDuty Admin</strong></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex mx-2">
+                        <button class="btn btn-light mx-1" type="submit"><a class="text-dark" href="./logout.php">Logout</a></button>
+                    </div>
                 </div>
-            </form>
-        </div>
-        <div class="right">
-            <img src="images/emblem.jpg" alt="emblem" id="image">
-        </div>
-    </div>
-    <?php } ?>
-</body>
+            </div>
+        </nav>
 
-</html>
+        <div class="container main_container">
+            <div class="row my-5 justify-content-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="card" style="height: 100%;">
+                        <div class="card-body">
+                            <div class="card-title py-3">
+                                <h1>OTP Confirmation</h1>
+                                <p class="card-text">Incorrect OTP entered</p>
+                                <form action="conformation.php" method="post" class="mt-5">
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control form-control-lg" placeholder="Enter OTP" name="captcha" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <button class="btn btn-primary btn-block">CONFIRM</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="container">
+                        <img src="./IndianEmblem.jpg" alt="Indian Emblem" id="emblem">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <footer class="mt-5 my_footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8">
+                        <h3>OnDuty</h3>
+                        <p>Your Safety, Our Priority.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-4">
+                        <h4>Contact Us </h4>
+                        <p><Strong>Email : </Strong> otd@gmail.com</p>
+                        <p><strong>Call us : </strong> +91 123456</p>
+                        <h4>About Us </h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus veritatis, sunt rerum velit eaque earum ex</p>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="copy text-center">
+                        <p><small>Copyright &copy; 2020. All Rights Reserved</small></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Bootstrap js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
+<?php } ?>
